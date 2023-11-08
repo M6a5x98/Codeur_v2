8b046c18f307ef54a1f55d3bf24c3e32780e92a4
M6a5x98 <130889212+M6a5x98@users.noreply.github.com>
2023-11-08 12:58:13 +0100

    Update dashboard.js

--- Dashboard/dashboard.js
+++ Dashboard/dashboard.js
@@ -1,1 +1,13 @@
+const url = "http://localhost:5000/discorddashboard";
 
+let request = {
+  addCommand: new XMLHttpRequest().open("PATCH", url, true),
+  moderation: {
+    forbinenWords: {
+      add: new XMLHttpRequest().open("PUT", url, true),
+      remove: new XMLHttpRequest().open("DELETE", url, true),
+      addCategory : new XMLHttpRequest().open("PATCH", url, true),
+      removeCategory: new XMLHttpRequest().open("DELETE", url, true)
+    },
+  },
+};
